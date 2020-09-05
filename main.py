a6e411a6f4c7642b0562246ee001d0be16090436
CutieMath <cutie.math@protonmail.com>
2020-09-05 13:47:10 +1000

    add image folder, finish main functions

--- main.py
+++ main.py
@@ -8,10 +8,12 @@
 # Spider Balloon: 128px <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
 # background:
 # bullet: 64px
+# death: Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
 
 
 import pygame
 import random
+import math
 
 # Initialise pygame and create game window
 pygame.init()
@@ -21,16 +23,16 @@ window = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Balloon Pop!")
 
 # Set up background
-background = pygame.image.load('background.png')
+background = pygame.image.load('image/background.png')
 
 # Player image -- a SPACESHIP!
-playerImg = pygame.image.load('spaceship.png')
+playerImg = pygame.image.load('image/spaceship.png')
 playerX = 667
 playerY = 268
 playerYChange = 0
 
 # balloon image -- added a spider so it looks scary!
-balloonImg = pygame.image.load('spider.png')
+balloonImg = pygame.image.load('image/spider.png')
 balloonX = 10
 balloonY = random.randint(0, 456)
 balloonYChange = 2
@@ -39,12 +41,15 @@ balloonYChange = 2
 # set bullet state for it's appearance
 # "ready" : hidden
 # "fire" : appear on screen
-bulletImg = pygame.image.load('bullet.png')
+bulletImg = pygame.image.load('image/bullet.png')
 bulletX = 667
 bulletY = 0
 bulletXChange = 5
 bulletState = "ready"
 
+
+# track score
+score = 0
 
 # use blit method to draw the spaceship & balloon on window
 def player(x, y):
@@ -59,6 +64,15 @@ def fire(x, y):
     global bulletState
     bulletState = "fire"
     window.blit(bulletImg, (x, y + 30))
+
+# calculate if the bullet and balloon collides
+# use Distance Formula (https://tinyurl.com/hgh57xt)
+def isCollision(balloonX, balloonY, bulletX, bulletY):
+    distance_D = math.sqrt( math.pow((balloonX - bulletX), 2) + math.pow((balloonY - bulletY), 2) )
+    if distance_D < 50:
+        return True
+    else:
+        return False
 
 
 # Set conditions for the game window to close
@@ -116,6 +130,16 @@ while running:
     if bulletState == "fire":
         fire(bulletX, bulletY)
         bulletX -= bulletXChange
+
+
+    # Collision
+    collision = isCollision(balloonX, balloonY, bulletX, bulletY)
+    if collision:
+        bulletX = 667
+        bulletState = "ready"
+        balloonImg = pygame.image.load('image/skull.png')
+        print(score)
+        score += 1
 
 
     # Draw the player and balloons after the screen background was set up
